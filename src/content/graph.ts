9abded938cc8898e99e5ff742df86cda865fbb8f
Petr Houška <houskape@gmail.com>
2020-04-18 10:50:36 +0200

    Graph uses humans.

--- src/content/graph.ts
+++ src/content/graph.ts
@@ -1,30 +1,31 @@
-type Person = string
+import { Human } from "./human"
+
 type NodeKey = string
 
 export class PeopleGraph {
-    private graph: Map<[Person, Person], number>
+    private graph: Map<[Human, Human], number>
 
     constructor(){
         this.graph = new Map()
     }
 
-    public setWeight(people: [Person, Person], weight: number){
+    public setWeight(people: [Human, Human], weight: number){
         let graphKey = this.getOrderedPair(people)
         this.graph[graphKey] = weight 
     }
 
-    public updateWeight(people: [Person, Person], weightDelta: number){
+    public updateWeight(people: [Human, Human], weightDelta: number){
         let graphKey = this.getOrderedPair(people)
         this.graph[graphKey] += weightDelta 
     }
 
-    public getWeight(people: [Person, Person]): number{
+    public getWeight(people: [Human, Human]): number{
         let graphKey = this.getOrderedPair(people)
         return this.graph[graphKey] 
     }
 
-    private getOrderedPair(unorderedPair: [Person, Person]): NodeKey{
-        let [a, b] = unorderedPair
+    private getOrderedPair(unorderedPair: [Human, Human]): NodeKey{
+        let [a, b] = unorderedPair.map(n => n.name)
         let orderedPair = a <= b ? [a, b] : [b, a]
 
         return orderedPair.join('|')
