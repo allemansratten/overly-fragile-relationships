9f5f9864736f7e53dc924141ed910d26674ad259
Petr Houška <houskape@gmail.com>
2020-04-18 13:53:06 +0200

    Integration of frienship manager.

--- src/content/friendshipManager.ts
+++ src/content/friendshipManager.ts
@@ -1,7 +1,8 @@
-import { HateGraph, Constrain } from "./hateGraph";
+import { HateGraph, Constrain, RelationShipEffect } from "./hateGraph";
 import { PeopleGraph } from "./peopleGraph";
 import { Human } from "./human";
 import { Location } from "./location"
+import { TripSummary } from "../management/tripsummary";
 
 export class FriendshipManager {
     public hateGraph: HateGraph
@@ -12,19 +13,38 @@ export class FriendshipManager {
         this.peopleGraph = peopleGraph
     }
 
-    public ApplyMeeting(peoplePresent: Array<Human>, location: Location) {
+    public ApplyMeeting(trip: TripSummary): Array<RelationShipEffect> {
+        let appliedEffects = []
+
         this.hateGraph.constrains.forEach(con => {
-            this.tryResolveConstrian(con, peoplePresent, location);
+            const appliedEffectsForCurrConstr = this.tryResolveConstrain(con, trip.goPeople, trip.goLocation);
+            appliedEffects = appliedEffects.concat(appliedEffectsForCurrConstr);
+
         });
+        return appliedEffects
     }
 
-    private tryResolveConstrian(con: Constrain, peoplePresent: Array<Human>, location: Location) {
+    private tryResolveConstrain(con: Constrain, peoplePresent: Array<Human>, location: Location): Array<RelationShipEffect> {
+
+        let appliedEffects = []
+
+        if (false){
+            // TODO: Figure out how to use debugger with this build system
+            console.log(con, peoplePresent, location)
+            console.log(con.haveToBePresent.every(hp => peoplePresent.some(pp => pp.name == hp.name)), peoplePresent, location)
+            console.log(con.cannotBePresent.every(cp => !peoplePresent.some(pp => pp.name == cp.name)))
+            console.log(con.allowedLocations.some(loc => loc.name == location.name))
+        }
+
         if (con.haveToBePresent.every(hp => peoplePresent.some(pp => pp.name == hp.name)) &&
             con.cannotBePresent.every(cp => !peoplePresent.some(pp => pp.name == cp.name)) &&
             con.allowedLocations.some(loc => loc.name == location.name)) {
             con.effect.forEach(eff => {
                 this.peopleGraph.updateWeight(eff.people, eff.relationshipChange);
+                appliedEffects.push(eff)
             });
         }
+
+        return appliedEffects;
     }
 }
