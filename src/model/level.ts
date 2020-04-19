48b1c1c1d5d7145df5920f727a209bbc8989650d
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-04-19 18:29:34 +0200

    work on #20

--- src/model/level.ts
+++ src/model/level.ts
@@ -44,17 +44,14 @@ export class Level {
         let effects = this.friendshipManager.applyMeeting(tripSummary)
 
         // Construct msgs for effects
-
-        let {perPersonRelMsg, perPersonHumMsg} = this.reduceEffectsPerPerson(effects)
-        let effectsMsgs = effects.map(effect => {
-            return effect.description
-        })
+        let { perPersonRelMsg, perPersonHumMsg } = this.reduceEffectsPerPerson(effects)
+        let effectsMsgs = Array.from(new Set(effects.map(effect => effect.description)))
         effectsMsgs.push("") // separator dummy
         effectsMsgs = effectsMsgs.concat(this.createEffectsMsgs(perPersonRelMsg, perPersonHumMsg))
 
         let effectMsg = effectsMsgs.length > 0
             ? effectsMsgs.join('\n')
-            : "No one cared for your trip. ╯︿╰"
+            : "Nothing significant occured."
 
         // Update relationships on people
         this.updateHumansFromPeopleGraphForDisplay()
@@ -64,7 +61,7 @@ export class Level {
         console.log("Relationships", this.friendshipManager.peopleGraph)
 
         // Construct final msg
-        let friendList: string = tripSummary.goPeople.map((human: Human) => human.name).join(', ')
+        let friendList: string = tripSummary.goPeople.filter((x: Human) => x.name != 'You').map((human: Human) => human.name).join(', ')
         let statusMessage = `You went out to ${tripSummary.goLocation} with ${friendList}.\n${effectMsg}`
 
         return statusMessage
@@ -72,25 +69,66 @@ export class Level {
 
     private createEffectsMsgs(
         perPersonRelMsg: Map<[HumanName, HumanName], [RelationshipTag[], RelationshipTag[]]>,
-        perPersonHumMsg: Map<HumanName, [HumanTag[], HumanTag[]]>) {
+        perPersonHumMsg: Map<HumanName, [HumanTag[], HumanTag[]]>
+    ) : Array<string> {
+        let effectMsg : Array<string> = []
 
-        let relMsgs = Array<string>()
-        perPersonRelMsg.forEach((changes, couple) => {
-            // TODO: empty tag names will not look nice, as well as empty newRelTags or oldRelTags
-            let newRelTags = changes[0].map(t => relationshipTagMap.get(t) ?? "").join(", ")
-            let oldRelTags = changes[1].map(t => relationshipTagMap.get(t) ?? "").join(", ")
-            relMsgs.push(`${couple[0]} now ${newRelTags} and no longer ${oldRelTags}  ${couple[1]}.`)
-        })
-
-        let humMsgs = Array<string>()
         perPersonHumMsg.forEach((changes, person) => {
-            let newHumTags = changes[0].map(t => humanTagMap.get(t) ?? "").join(", ")
-            let oldHumTags = changes[1].map(t => humanTagMap.get(t) ?? "").join(", ")
-            relMsgs.push(`${person} is now ${newHumTags} and no longer ${oldHumTags}.`)
+            let newTags = changes[0]
+            let remTags = changes[1]
+            let remTagsArr = []
+            for(let remTag of remTags) {
+                if(humanTagMap.has(remTag)) {
+                    remTagsArr.push(humanTagMap.get(remTag))
+                }
+            }
+            let newTagsArr = []
+            for(let newTag of newTags) {
+                if(humanTagMap.has(newTag)) {
+                    newTagsArr.push(humanTagMap.get(newTag))
+                }
+            }
+            if(remTagsArr.length != 0 && newTagsArr.length != 0) {
+                effectMsg.push(`${person} lost ${remTagsArr.join(', ')}, but gained ${newTagsArr.join(', ')}`)
+            } else if(remTagsArr.length != 0) {
+                effectMsg.push(`${person} lost ${remTagsArr.join(', ')}`)
+            } else if(newTagsArr.length != 0) {
+                effectMsg.push(`${person} gained ${newTagsArr.join(', ')}`)
+            }
         })
-        let effectsMsgs = relMsgs.concat(humMsgs)
 
-        return effectsMsgs
+        perPersonRelMsg.forEach((changes, couple) => {
+            let newTags = changes[0]
+            let remTags = changes[1]
+            for(let remTag of remTags) {
+                if(relationshipTagMap.has(remTag)) {
+                    effectMsg.push(`${couple[0]} no longer ${relationshipTagMap.get(remTag)} ${couple[1]}`)
+                }
+            }
+            for(let newTag of newTags) {
+                if(relationshipTagMap.has(newTag)) {
+                    effectMsg.push(`${couple[0]} now ${relationshipTagMap.get(newTag)} ${couple[1]}`)
+                }
+            }
+        })
+        
+        // let relMsgs = Array<string>()
+        // perPersonRelMsg.forEach((changes, couple) => {
+        //     // TODO: empty tag names will not look nice, as well as empty newRelTags or oldRelTags
+        //     let newRelTags = changes[0].map(t => relationshipTagMap.get(t) ?? "").join(", ")
+        //     let oldRelTags = changes[1].map(t => relationshipTagMap.get(t) ?? "").join(", ")
+        //     relMsgs.push(`${couple[0]} now ${newRelTags} and no longer ${oldRelTags}  ${couple[1]}.`)
+        // })
+
+        // let humMsgs = Array<string>()
+        // perPersonHumMsg.forEach((changes, person) => {
+        //     let newHumTags = changes[0].map(t => humanTagMap.get(t) ?? "").join(", ")
+        //     let oldHumTags = changes[1].map(t => humanTagMap.get(t) ?? "").join(", ")
+        //     relMsgs.push(`${person} is now ${newHumTags} and no longer ${oldHumTags}.`)
+        // })
+        // let effectMsg = relMsgs.concat(humMsgs)
+
+        return effectMsg
     }
 
     private reduceEffectsPerPerson(effects: SituationEffect[]) {
@@ -98,7 +136,7 @@ export class Level {
         let perPersonRelMsg = new Map<[HumanName, HumanName], [Array<RelationshipTag>, Array<RelationshipTag>]>()
         let perPersonHumMsg = new Map<HumanName, [Array<HumanTag>, Array<HumanTag>]>()
 
-        let addToMap = function<K, V>(key: K, value: V, valueStore: Map<K, [Array<V>, Array<V>]>, addedRemovedStoreSwitch: 0|1) {
+        let addToMap = function <K, V>(key: K, value: V, valueStore: Map<K, [Array<V>, Array<V>]>, addedRemovedStoreSwitch: 0 | 1) {
             let perKeyStore = valueStore.get(key) ?? [new Array<V>(), new Array<V>()]
             perKeyStore[addedRemovedStoreSwitch].push(value)
             valueStore.set(key, perKeyStore)
